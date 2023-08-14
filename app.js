8304973e51ad9134a63561ffc999430d5d9b30bf
jeal0uspengu1n <sutej.sharma@yahoo.com>
2023-08-14 18:51:32 +0530

    Implemented the reply feature

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const ejs = require("ejs");
 const _= require("lodash");
 const fs = require('fs');
 
-const homeStartingContent = "I need some more information about the services offered by Branch International";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
@@ -23,6 +22,7 @@ app.use(express.static("public"));
 
 // creating the map and storing the user messages uniquely along with timestamps
 map = new Map();
+adminReply = new Map();
 map.set(0, []);
 map.get(0).push({
   TimestampUTC: "2017-02-01 19:21:58",
@@ -41,21 +41,33 @@ for(var i=0; i<dataArray.length; i++)
 }
 // end of map thing
 
-var obj= {
-  entryMessage: map.get(0)[0].MessageBody,
-  entryUserID: "User 0",
-  entryTimestamp: map.get(0)[0].TimestampUTC
-};
-var s= _.truncate(map.get(0)[0].MessageBody,{
-  length: 69
+var objEntry=[];
+var objEntry1=[];
+
+map.forEach((values, key) => {
+  // console.log(key);
+  var MessageBodyArr = [];
+  var TimestampUTCArr = [];
+  values.forEach((value) => {
+    MessageBodyArr.push(value.MessageBody);
+    TimestampUTCArr.push(value.TimestampUTC);
+  });
+  var obj= {
+    entryMessage: MessageBodyArr,
+    entryUserID: "User" + key,
+    entryTimestamp: TimestampUTCArr
+  };
+  var s= _.truncate(MessageBodyArr[0],{
+    length: 69
+  });
+  var OBJ={
+    entryMessage: s,
+    entryUserID: "User" + key,
+    entryTimestamp: TimestampUTCArr
+  };
+  objEntry.push(obj);
+  objEntry1.push(OBJ);
 });
-var OBJ={
-  entryMessage: s,
-  entryUserID: "User 0",
-  entryTimestamp: map.get(0)[0].TimestampUTC
-};
-var objEntry=[obj];
-var objEntry1=[OBJ];
 
 app.get("/",function(req,res){
   res.render("home",{objEntry1: objEntry1});
@@ -77,39 +89,37 @@ app.get("/compose",function(req,res){
 });
 
 app.post("/compose",function(req,res){
-  var entryMessage= req.body.newEntry;
-  var entryUserID= req.body.newEntryTitle;
-  var entryTimestamp= "0000-00-00 00:00:00"
-  // console.log(entry);
-  var ss= _.truncate(entryMessage, {
-    length: 69
-  });
-  var obj1={
-    entryMessage: entryMessage,
-    entryUserID: entryUserID,
-    entryTimestamp: entryTimestamp
-  };
-  var obj2={
-    entryMessage: ss,
-    entryUserID: entryUserID,
-    entryTimestamp: entryTimestamp
-  };
-  objEntry.push(obj1);
-  objEntry1.push(obj2)
   res.redirect("/");
 });
 
 
 app.get("/post/:key",function(req,res){
-  // console.log(req.params.key);
+  // console.log("req.qeury.data", req.query.data);
+  var data = [];
+  if(req.query.data !== undefined) {
+    data = JSON.parse(req.query.data);
+  }
   var s1= _.lowerCase(req.params.key);
+  if(adminReply.get(s1) === undefined)
+  {
+    adminReply.set(s1,[]);
+  }
+  var adminReplyArr = data;
   var f=-11;
   objEntry.forEach(function(obj){
     var s2= _.lowerCase(obj.entryUserID);
     if(s1===s2)
     {
-      res.render("post", {UserID: obj.entryUserID, MessageBody: obj.entryMessage, TimestampUTC: obj.entryTimestamp,});
-      console.log(obj);
+      const postRoute = "/post/:User" + s1.substring(5);
+      // console.log("postRoute", postRoute);
+      res.render("post", {
+        UserID: obj.entryUserID, 
+        MessageBody: obj.entryMessage, 
+        TimestampUTC: obj.entryTimestamp, 
+        adminReplyArr: adminReplyArr,
+        postRoute: postRoute
+      });
+      // console.log(obj);
       f=1;
     }
   });
@@ -117,6 +127,19 @@ app.get("/post/:key",function(req,res){
   {
     res.render("failure");
   }
+});
+
+app.post("/post/:key", function(req, res) {
+  let userId = req.body.UserID.substring(4);
+  if(adminReply.get(userId) === undefined) {
+    adminReply.set(userId, []);
+  }
+  const currentTime = new Date();
+  let timeStamp = currentTime.toISOString();
+  timeStamp = timeStamp.substring(0, 10) + " " + timeStamp.substring(11, 19);
+  adminReply.get(userId).push(req.body.newEntry + " (" + timeStamp + ")");
+  const data = JSON.stringify(adminReply.get(userId));
+  res.redirect(`/post/:${req.body.UserID}?data=${data}`)
 });
 
 app.listen(process.env.PORT || 3000, function() {
